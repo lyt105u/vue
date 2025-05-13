6bf2161a92191f2dfe538cde7932dd0599128620
lab <a25562147@gmail.com>
2025-05-13 16:16:11 +0800

    Modify Home page:    Fix icon no show problem, and remove HelloWorld component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,16 +5,15 @@
     <p>presented on 2025/04/30</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
