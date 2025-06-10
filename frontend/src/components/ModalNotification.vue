2b9d827fc07731a5c65c730394e7809f1e5da520
home <a25562147@gmail.com>
2025-06-10 20:59:28 +0800

    Modify ModalNotification.vue:    Clicking outside the modal doesn't close it

--- frontend/src/components/ModalNotification.vue
+++ frontend/src/components/ModalNotification.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="modal fade" ref="modalRef" tabindex="-1" aria-labelledby="finishTrainingModalLabel" aria-hidden="true">
+  <div class="modal fade" ref="modalRef" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="finishTrainingModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
