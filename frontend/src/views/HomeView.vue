c9886d99d1dfc7b8226a450d96a7aa9ab1404c2c
lab <a25562147@gmail.com>
2025-06-13 13:26:29 +0800

    Modify Home page:    Add new logo and favicon

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo2.png" style="max-width: 400px; max-height: 400px;">
-    <h1></h1>
-    <p>2025/04/30</p>
+  <div class="home" style="min-height: auto;">
+    <img alt="Vue logo" src="../assets/logo4.png" style="max-width: 200px; max-height: 200px;">
+    <h1 class="display-5 fw-bold text-body-emphasis">MLAS</h1>
+    <p>Machine Learning Analysis System</p>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <select class="form-select" v-model="$i18n.locale">
       <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ localeLabels[locale] }}</option>
     </select>
+  </div>
+  <div class="container">
+    <footer class="flex-wrap align-items-center py-3 my-4 border-top">
+      <p class="text-center text-body-secondary">&copy; 2025 Network & System Lab, NSYSU | Designed by Yen-Ting Lai</p>
+    </footer>
   </div>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </template>
